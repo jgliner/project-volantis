d40275dff461f4b1f7e2c21234862f6d2a70f1b3
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-04-16 15:44:59 -0700

    Define static path and routes file

---
+++ server/routes.js
@@ -0,0 +1,8 @@
+'use strict';
+const path = require('path');
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+}
--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+// relative path for static assets only
+app.use(express.static(path.join(__dirname, '../client')));
+
+require('./routes.js')(app, express);
+
 const port = process.env.PORT || 7001;
 
 console.log(`server running on port ${port}`);
